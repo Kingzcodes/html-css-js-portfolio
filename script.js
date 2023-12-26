f75c4f51bc70bce6b3a2ef296ed17cb4778a41d9
Kingzcodes <Kingzcode29@gmail.com>
2023-12-26 11:02:35 +0100

    updates

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-zx
+function togglemenu(){
+    const menu = document.queryselector(".menu-links");
+    const icon = document.queryselector(".hamburger-icon");
+    menu.classlist.toggle("open");
+    icon.classlist.toggle("open");
+    
+}
